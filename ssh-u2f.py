ec86e6772c64ab0d25695f51a51098635672b595
Brandon Smith <brandon@praesid.io>
2016-03-04 09:45:30 -0800

    Make OTP prompt configurable

--- ssh-u2f.py
+++ ssh-u2f.py
@@ -31,9 +31,12 @@ interact with privacyIDEA's pam_python module for u2f challenge/response.
 
 Usage:
     Make executable
+    Configure otp_prompt variable to match your pam prompt
     Symlink ssh-u2f, scp-u2f, sftp-u2f, mosh-u2f, etc. into your PATH
     Call just like ssh, eg. "ssh-u2f name@example.com"
 """
+
+otp_prompt = "privacyIDEA_Authentication"
 
 ssh = None
 
@@ -75,7 +78,7 @@ def passthrough():
 
 while True:
     index = ssh.expect(["Authenticated with partial success.",
-                        "([Pp]assword[^:\r\n]*|Enter additional factors): ?",
+                        "([Pp]assword[^:\r\n]*|%s): ?" % otp_prompt,
                         "----- BEGIN U2F CHALLENGE -----\r\n",
                         "[^ \r\n]+",
                         pexpect.EOF])
