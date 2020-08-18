7874605ebfbb9263893124bee34f96d756717364
Quentin Lux <luxquent@mila.quebec>
2020-08-18 12:54:47 -0400

    test

--- tests/test_pam_module.py
+++ tests/test_pam_module.py
@@ -185,6 +185,9 @@ class PAMTestCase(unittest.TestCase):
                 "sqlfile=%s" % SQLFILE,
                 "try_first_pass"]
         r = pam_sm_authenticate(pamh, flags, argv)
+        print(pamh)
+        print(pamh.authtok)
+        print(pamh.conversation(""))
         self.assertEqual(r, PAMH.PAM_SUCCESS)
 
         # Now there are no offline values left
